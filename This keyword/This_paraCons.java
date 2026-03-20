111a11f942516e8800589989d74143853b189061
Parth <parthagarai001@gmail.com>
2026-03-20 22:23:12 +0530

    calling parameterized constructor using this()

---
+++ This keyword/This_paraCons.java
@@ -0,0 +1,12 @@
+class This_paraCons{
+    This_paraCons(){
+        this(8);
+    }
+    This_paraCons(int a){
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        This_paraCons r = new This_paraCons();
+        
+    }
+}
