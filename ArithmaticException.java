5ab8472c74b28cf45b6a53736b821a44fad20109
Parth <parthagarai001@gmail.com>
2026-04-20 21:07:56 +0530

    Add files via upload

---
+++ ArithmaticException.java
@@ -0,0 +1,15 @@
+class ArithmaticException{
+    public static void main(String[] args) {
+        System.out.println("Main method started ");
+        int a=10,b=0,c;
+        try{
+            c=a/b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("main method ended");
+        
+    }
+}
